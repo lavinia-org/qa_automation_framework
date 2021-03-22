a6a9f8bc45e88265f0a4705af1d8f75ce2e1572b
Larisa Alexa <larisa.alexa@softvision.com>
2021-03-22 17:00:53 +0200

    added log4j dependencies, updated BaseTest to setup Log4j and added checkBrowserOS method

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,18 +1,28 @@
 package base;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
     protected WebDriver driver;
+    protected Logger log;
 
     @BeforeMethod (alwaysRun = true)
-    public void setDriver() {
+    public void setDriver(ITestContext context) {
+        String testName = context.getCurrentXmlTest().getName();
+        log = LogManager.getLogger(testName);
+
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         driver = new ChromeDriver();
+        checkBrowserOS();
         driver.manage().window().maximize();
     }
 
@@ -23,5 +33,15 @@ public class BaseTest {
 
     public void openURL(String URL) {
         driver.get(URL);
+    }
+
+    /** Get browser name, browser version and OS */
+    public  void checkBrowserOS() {
+        Capabilities capabilities = ((RemoteWebDriver) driver).getCapabilities();
+        String browserName = capabilities.getBrowserName();
+        String browserVersion = capabilities.getVersion();
+        String os = System.getProperty("os.name");
+
+        log.info("Initializing driver: " + browserName + " " + browserVersion + " on " + os);
     }
 }
